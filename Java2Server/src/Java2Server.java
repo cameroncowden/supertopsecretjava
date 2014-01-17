72de36d76ff8b1b8a06fbe8f296ad0013d91a12e
cameroncowden <cameroncowden@tamu.edu>
2014-01-17 16:23:42 -0600

    Adding a try and catch and exception handler.

--- Java2Server/src/Java2Server.java
+++ Java2Server/src/Java2Server.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -7,14 +8,24 @@ public class Java2Server {
 		
 		int portNumber = 5555;
 		int queueLength = 10;
-		
+		try{
 		ServerSocket server = new ServerSocket(portNumber, queueLength); 
-		Socket connection = server.accept();
 		
+		Socket connection = server.accept(); //Accept a connection from a client
+		
+		//Receive two ints from client
+		//sent the two ints to SumTheseTwo, which returns their sum
+		//Send the sum to the client.
 		connection.getOutputStream();
 		connection.getInputStream();
 		
 		
+		
+		}
+		catch(IOException e){
+			e.printStackTrace();
+			return;
+		}
 	}
 	
 	public int SumTheseTwo(int a, int b){
